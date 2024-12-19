2a6f51526b09804bc0cf2d6c7a9d9ce2522217f0
Richard Kang <2356020+kangks@users.noreply.github.com>
2024-12-19 16:45:08 +0800

    added simple greeting intent with first name slot

--- lib/constructs/lex-bot/lambda/lex-fallback/index.py
+++ lib/constructs/lex-bot/lambda/lex-fallback/index.py
@@ -1,6 +1,7 @@
 import json
 import os
-import logging
+# import logging
+from aws_lambda_powertools import Logger, Tracer
 import boto3
 import uuid
 from botocore.config import Config
@@ -8,12 +9,13 @@ from botocore.config import Config
 # from classes import bedrock_agent_runtime_wrapper 
 from botocore.exceptions import ClientError
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+tracer = Tracer()
+logger = Logger()
 config = Config(read_timeout=1000)
 bedrock_agent = boto3.client('bedrock-agent-runtime', config=config)
 
 # --- Intent handler ---
+@tracer.capture_method
 def handle_fallback(intent_request, context):
     logger.debug("intent request for fallback:%s", json.dumps(intent_request))
     
@@ -24,7 +26,7 @@ def handle_fallback(intent_request, context):
         
         # Get response from Bedrock agent
         agent_response = invoke_bedrock_agent(input_text)
-        
+        logger.debug("agent_response=%s",agent_response)
         # Format response for Lex
         return {
             "sessionState": {
@@ -44,7 +46,7 @@ def handle_fallback(intent_request, context):
             ]
         }
     except Exception as e:
-        logger.error(f"Error handling Lex event: {str(e)}")
+        logger.exception(f"Error handling Lex event: {str(e)}")
         return {
             "sessionState": {
                 "dialogAction": {
@@ -63,27 +65,30 @@ def handle_fallback(intent_request, context):
             ]
         }
     
+@tracer.capture_method
 def invoke_bedrock_agent(input_text):
-    bedrock_agent = boto3.client('bedrock-agent-runtime')
-    wrapper = BedrockAgentRuntimeWrapper(bedrock_agent)
-
-    agent_id = os.environ['BEDROCK_AGENT_ID']
-    agent_alias_id = os.environ['BEDROCK_AGENT_ALIAS_ID']
-    session_id = str(uuid.uuid4())
-    prompt = input_text
 
     try:
-        response =  wrapper.invoke_agent(agent_id, agent_alias_id, session_id, prompt)
+        bedrock_agent = boto3.client('bedrock-agent-runtime')
+        enable_bedrock_agent_trace = os.getenv('ENABLE_BEDROCK_AGENT_TRACE', False)
 
+        agent_id = os.environ['BEDROCK_AGENT_ID']
+        agent_alias_id = os.environ['BEDROCK_AGENT_ALIAS_ID']
+        session_id = str(uuid.uuid4())
+        prompt = input_text
+        logger.debug("agent_id=%s, agent_alias_id=%s, session_id=%s, prompt=%s", agent_id, agent_alias_id, session_id, prompt)
+        wrapper = BedrockAgentRuntimeWrapper(bedrock_agent, enable_bedrock_agent_trace=True)
+        response =  wrapper.invoke_agent(agent_id, agent_alias_id, session_id, prompt)
         logger.debug("response:%s", response)
 
         return response
     except Exception as e:
-        logger.error(f"Error invoking Bedrock agent: {str(e)}")
-        return "I apologize, but I'm having trouble accessing the book information right now."
+        logger.exception(f"Error invoking Bedrock agent: {str(e)}")
+        return "I apologize, but I'm having trouble accessing the restaurant agent right now."
 
 # --- Intents ---
-
+@logger.inject_lambda_context
+@tracer.capture_lambda_handler
 def lambda_handler(event, context):
     """
     Route the incoming request based on intent.
@@ -97,17 +102,19 @@ def lambda_handler(event, context):
 class BedrockAgentRuntimeWrapper:
     """Encapsulates Amazon Bedrock Agents Runtime actions."""
 
-    def __init__(self, runtime_client):
+    def __init__(self, runtime_client, enable_bedrock_agent_trace):
         """
         :param runtime_client: A low-level client representing the Amazon Bedrock Agents Runtime.
                                Describes the API operations for running
                                inferences using Bedrock Agents.
         """
         self.agents_runtime_client = runtime_client
+        self.enable_bedrock_agent_trace = enable_bedrock_agent_trace
 
     # snippet-end:[python.example_code.bedrock-agent-runtime.BedrockAgentRuntimeWrapper.decl]
 
     # snippet-start:[python.example_code.bedrock-agent-runtime.InvokeAgent]
+    @tracer.capture_method
     def invoke_agent(self, agent_id, agent_alias_id, session_id, prompt):
         """
         Sends a prompt for the agent to process and respond to.
@@ -120,7 +127,7 @@ class BedrockAgentRuntimeWrapper:
         :return: Inference response from the model.
         """
 
-        logger.debug("agent_id=%s, agent_alias_id=%s, session_id=%s, prompt=%s", agent_id, agent_alias_id, session_id, prompt)
+        logger.debug("agent_id=%s, agent_alias_id=%s, session_id=%s, prompt=%s, enable_bedrock_agent_trace=%s", agent_id, agent_alias_id, session_id, prompt, self.enable_bedrock_agent_trace)
 
         try:
             # Note: The execution time depends on the foundation model, complexity of the agent,
@@ -131,16 +138,49 @@ class BedrockAgentRuntimeWrapper:
                 agentAliasId=agent_alias_id,
                 sessionId=session_id,
                 inputText=prompt,
+                enableTrace=True
             )
 
-            completion = ""
+            logger.debug("response=%s", response)
 
+            completion = ""
+            citations = []
+            trace = {}
+
+            has_guardrail_trace = False
             for event in response.get("completion"):
-                chunk = event["chunk"]
-                completion = completion + chunk["bytes"].decode()
+                # Combine the chunks to get the output text
+                if "chunk" in event:
+                    chunk = event["chunk"]
+                    completion += chunk["bytes"].decode()
+                    if "attribution" in chunk:
+                        citations = citations + chunk["attribution"]["citations"]
+
+                # Extract trace information from all events
+                if "trace" in event:
+                    for trace_type in ["guardrailTrace", "preProcessingTrace", "orchestrationTrace", "postProcessingTrace"]:
+                        if trace_type in event["trace"]["trace"]:
+                            mapped_trace_type = trace_type
+                            if trace_type == "guardrailTrace":
+                                if not has_guardrail_trace:
+                                    has_guardrail_trace = True
+                                    mapped_trace_type = "preGuardrailTrace"
+                                else:
+                                    mapped_trace_type = "postGuardrailTrace"
+                            if trace_type not in trace:
+                                trace[mapped_trace_type] = []
+                            trace[mapped_trace_type].append(event["trace"]["trace"][trace_type])
+
+            logger.debug(f"completion: {completion}")
+            logger.debug(f"trace: {trace}")
 
         except ClientError as e:
-            logger.error(f"Couldn't invoke agent. {e}")
+            logger.exception(f"Couldn't invoke agent. {e}")
             raise
 
         return completion
+        # return {
+        #     "output_text": completion,
+        #     "citations": citations,
+        #     "trace": trace
+        # }
--- lib/constructs/lex-bot/lex-bot-construct.ts
+++ lib/constructs/lex-bot/lex-bot-construct.ts
@@ -20,81 +20,83 @@ export class LexBotConstruct extends Construct {
 
     const lexIntentName = "lexBedrockKB";
 
-    const lexRole = new cdk.aws_iam.Role(this, 
+    const lexRole = new cdk.aws_iam.Role(this,
       `${cdk.Stack.of(this).stackName}-LexRole`,
       {
-      assumedBy: new cdk.aws_iam.ServicePrincipal('lex.amazonaws.com'),
-      inlinePolicies: {
-        'LexBasicExecution': new cdk.aws_iam.PolicyDocument({
-          statements: [
-            new cdk.aws_iam.PolicyStatement({
-              effect: cdk.aws_iam.Effect.ALLOW,
-              actions: [
-                'polly:SynthesizeSpeech',
-              ],
-              resources: ['*']
-            }),
-            new cdk.aws_iam.PolicyStatement({
-              effect: cdk.aws_iam.Effect.ALLOW,
-              actions: [
-                'lex:PutIntent',
-              ],
-              resources: [
-                `arn:aws:lex:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:intent:${lexIntentName}:*`,
-              ]
-            }),
-          ]
-        })
-      }
-    });
+        assumedBy: new cdk.aws_iam.ServicePrincipal('lex.amazonaws.com'),
+        inlinePolicies: {
+          'LexBasicExecution': new cdk.aws_iam.PolicyDocument({
+            statements: [
+              new cdk.aws_iam.PolicyStatement({
+                effect: cdk.aws_iam.Effect.ALLOW,
+                actions: [
+                  'polly:SynthesizeSpeech',
+                ],
+                resources: ['*']
+              }),
+              new cdk.aws_iam.PolicyStatement({
+                effect: cdk.aws_iam.Effect.ALLOW,
+                actions: [
+                  'lex:PutIntent',
+                ],
+                resources: [
+                  `arn:aws:lex:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:intent:${lexIntentName}:*`,
+                ]
+              }),
+            ]
+          })
+        }
+      });
 
-    const lexFallbackFunctionLambdaRole = new cdk.aws_iam.Role(this, 
+    const lexFallbackFunctionLambdaRole = new cdk.aws_iam.Role(this,
       `${cdk.Stack.of(this).stackName}-lexfallback-lambdarole`,
       {
-      assumedBy: new cdk.aws_iam.ServicePrincipal('lambda.amazonaws.com'),
-      inlinePolicies: {
-        'LambdaBasicExecution': new cdk.aws_iam.PolicyDocument({
-          statements: [
-            new cdk.aws_iam.PolicyStatement({
-              effect: cdk.aws_iam.Effect.ALLOW,
-              actions: [
-                'logs:CreateLogGroup',
-                'logs:CreateLogStream',
-                'logs:PutLogEvents'
-              ],
-              resources: [
-                `arn:aws:logs:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:log-group:/aws/lambda/*`
-              ]
-            }),
-            new cdk.aws_iam.PolicyStatement({
-              effect: cdk.aws_iam.Effect.ALLOW,
-              actions: [
-                'bedrock:InvokeAgent',
-              ],
-              resources: [
-                `arn:aws:bedrock:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:agent-alias/${props.bedrockAgentId}/*`
-              ]
-            })
-          ]
-        })
-      }
-    });
+        assumedBy: new cdk.aws_iam.ServicePrincipal('lambda.amazonaws.com'),
+        inlinePolicies: {
+          'LambdaBasicExecution': new cdk.aws_iam.PolicyDocument({
+            statements: [
+              new cdk.aws_iam.PolicyStatement({
+                effect: cdk.aws_iam.Effect.ALLOW,
+                actions: [
+                  'logs:CreateLogGroup',
+                  'logs:CreateLogStream',
+                  'logs:PutLogEvents'
+                ],
+                resources: [
+                  `arn:aws:logs:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:log-group:/aws/lambda/*`
+                ]
+              }),
+              new cdk.aws_iam.PolicyStatement({
+                effect: cdk.aws_iam.Effect.ALLOW,
+                actions: [
+                  'bedrock:InvokeAgent',
+                ],
+                resources: [
+                  `arn:aws:bedrock:${cdk.Stack.of(this).region}:${cdk.Stack.of(this).account}:agent-alias/${props.bedrockAgentId}/*`
+                ]
+              })
+            ]
+          })
+        }
+      });
 
-    const lexFallbackFunction = new lambda_python.PythonFunction(this, 
+    const lexFallbackFunction = new lambda_python.PythonFunction(this,
       `${cdk.Stack.of(this).stackName}-lexfallback-lambdafunction`,
       {
-      runtime: lambda.Runtime.PYTHON_3_13,
-      handler: 'lambda_handler',
-      entry: path.join(__dirname, './lambda/lex-fallback'),
-      layers: [lambda.LayerVersion.fromLayerVersionArn(this, 'LexFallbackPowerToolsLayer',
-        `arn:aws:lambda:${cdk.Stack.of(this).region}:017000801446:layer:AWSLambdaPowertoolsPythonV3-python313-x86_64:4`)],
-      timeout: cdk.Duration.minutes(2),
-      role: lexFallbackFunctionLambdaRole,
-      environment: {
-        BEDROCK_AGENT_ID: props.bedrockAgentId,
-        BEDROCK_AGENT_ALIAS_ID: props.bedrockAgentAliasId
-      }
-    });
+        runtime: lambda.Runtime.PYTHON_3_13,
+        handler: 'lambda_handler',
+        entry: path.join(__dirname, './lambda/lex-fallback'),
+        layers: [lambda.LayerVersion.fromLayerVersionArn(this, 'LexFallbackPowerToolsLayer',
+          `arn:aws:lambda:${cdk.Stack.of(this).region}:017000801446:layer:AWSLambdaPowertoolsPythonV3-python313-x86_64:4`)],
+        timeout: cdk.Duration.minutes(2),
+        role: lexFallbackFunctionLambdaRole,
+        environment: {
+          BEDROCK_AGENT_ID: props.bedrockAgentId,
+          BEDROCK_AGENT_ALIAS_ID: props.bedrockAgentAliasId,
+          POWERTOOLS_LOG_LEVEL: "DEBUG",
+          ENABLE_BEDROCK_AGENT_TRACE: "True"
+        }
+      });
 
     lexFallbackFunction.grantInvoke(new cdk.aws_iam.ServicePrincipal('lexv2.amazonaws.com'))
     NagSuppressions.addResourceSuppressions(lexFallbackFunctionLambdaRole, [{
@@ -124,85 +126,112 @@ export class LexBotConstruct extends Construct {
     };
 
     // Create Lex bot with fallback intent
-    this.lexBot = new lex.CfnBot(this, 
+    this.lexBot = new lex.CfnBot(this,
       `${cdk.Stack.of(this).stackName}-lexbot`,
       {
-      name: lexIntentName,
-      roleArn: lexRole.roleArn,
-      dataPrivacy: {
-        ChildDirected: true
-      },
-      idleSessionTtlInSeconds: 300,
-      botLocales: [{
-        localeId: 'en_US',
-        nluConfidenceThreshold: 0.40,
-        voiceSettings: {
-          voiceId: 'Joanna'
+        name: lexIntentName,
+        roleArn: lexRole.roleArn,
+        dataPrivacy: {
+          ChildDirected: true
         },
-        intents: [{
-          name: lexIntentName,
-          description: 'Greeting',
-          sampleUtterances: [
+        idleSessionTtlInSeconds: 300,
+        botLocales: [{
+          localeId: 'en_US',
+          nluConfidenceThreshold: 0.40,
+          voiceSettings: {
+            voiceId: 'Joanna'
+          },
+          intents: [
             {
-              utterance: 'Hi'
+              name: 'WelcomeIntent',
+              sampleUtterances: [
+                { 'utterance': 'Hello' }
+              ],
+              slots:[
+                {
+                  name: "firstname",
+                  slotTypeName: "AMAZON.FirstName",
+                  valueElicitationSetting: {
+                    slotConstraint: "Required",
+                    promptSpecification: {
+                      messageGroupsList: [
+                          {
+                              message: {
+                                plainTextMessage: {
+                                  value: "Hello, what's your name?"
+                                }
+                              }
+                          },
+                      ],
+                      "maxRetries": 5,
+                      "allowInterrupt": true,
+                    },
+                  }
+                }
+              ],
+              slotPriorities: [
+                { priority: 1, slotName: 'firstname' },
+              ],
+              intentClosingSetting: {
+                closingResponse: {
+                  messageGroupsList: [{
+                    message: {
+                      plainTextMessage: { value: 'Hello {firstname}, how can I help you?' },
+                    },
+                  }],
+                }
+              }
             },
             {
-              utterance: 'How are you'
+              name: 'FallbackIntent', // Explicitly reference fallback intent
+              description: 'Default fallback intent',
+              parentIntentSignature: 'AMAZON.FallbackIntent',
+              fulfillmentCodeHook: { enabled: true },
             }
           ],
-          fulfillmentCodeHook: {
-            enabled: true
-          }
-        },
-        {
-          name: 'FallbackIntent', // Explicitly reference fallback intent
-          description: 'Default fallback intent',
-          parentIntentSignature: 'AMAZON.FallbackIntent',
-          fulfillmentCodeHook: { enabled: true },
-        }
-        ]
-      }],
-      autoBuildBotLocales: true,
-      testBotAliasSettings: testBotAliasSettingsProperty
-    });
+          description: 'Lex Bedrock Test Bot'
+        }],
+        autoBuildBotLocales: true,
+        testBotAliasSettings: testBotAliasSettingsProperty
+      });
 
     // Publish a numeric version of the bot
-    const botVersion = new lex.CfnBotVersion(this, 
+    const botVersion = new lex.CfnBotVersion(this,
       `${cdk.Stack.of(this).stackName}-lexbotversion`,
       {
-      botId: this.lexBot.attrId, // Reference the bot ID
-      botVersionLocaleSpecification: [
-        {
-          localeId: 'en_US', // Specify the locale
-          botVersionLocaleDetails: {
-            sourceBotVersion: 'DRAFT', // Publish from the DRAFT version
+        botId: this.lexBot.attrId, // Reference the bot ID
+        botVersionLocaleSpecification: [
+          {
+            localeId: 'en_US', // Specify the locale
+            botVersionLocaleDetails: {
+              sourceBotVersion: 'DRAFT', // Publish from the DRAFT version
+            },
           },
-        },
-      ],
-    });
+        ],
+      });
 
     // Associate Lambda with Lex Bot
-    const botAlias = new lex.CfnBotAlias(this, 
+    const botAlias = new lex.CfnBotAlias(this,
       `${cdk.Stack.of(this).stackName}-lexbotalias`,
       {
-      botId: this.lexBot.attrId,
-      botAliasName: 'TestChatBotAlias',
-      botVersion: botVersion.attrBotVersion,
-      botAliasLocaleSettings: [
-        {
-          localeId: 'en_US', // Specify the locale
-          botAliasLocaleSetting: {
-            enabled: true, // Enable the locale
-            codeHookSpecification: {
-              lambdaCodeHook: {
-                codeHookInterfaceVersion: '1.0', // Lambda interface version
-                lambdaArn: lexFallbackFunction.functionArn, // ARN of the Lambda function
+        botId: this.lexBot.attrId,
+        botAliasName: 'TestChatBotAlias',
+        botVersion: botVersion.attrBotVersion,
+        botAliasLocaleSettings: [
+          {
+            localeId: 'en_US', // Specify the locale
+            botAliasLocaleSetting: {
+              enabled: true, // Enable the locale
+              codeHookSpecification: {
+                lambdaCodeHook: {
+                  codeHookInterfaceVersion: '1.0', // Lambda interface version
+                  lambdaArn: lexFallbackFunction.functionArn, // ARN of the Lambda function
+                },
               },
             },
           },
-        },
-      ],
-    });
+        ],
+      });
 
     // Lex bot alias output
     new cdk.CfnOutput(this, 'LexBotAlias', {
